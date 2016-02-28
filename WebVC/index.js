b81d4b781ee6bcca0de39c08a688814f16bf5bef
Joseph Cruz <cruzj6@wit.edu>
2016-02-28 17:51:28 -0500

    Steam sign in complete, welcome page when not logged in

--- WebVC/index.js
+++ WebVC/index.js
@@ -24,8 +24,16 @@ app.controller('mainCtrl', function($scope, $http){
             $scope.searchResults = res.data;
         });
     };
-    $scope.getArticles = function(res){
+    $scope.getGameInfo= function(res){
+        for(var i = 0; i < $scope.trackedGames.length; i++)
+        {
+            $scope.trackedGames[i].curColor = 'white';
+            $scope.trackedGames[i].textColor = 'black';
+        }
+        res.textColor = 'white';
+        res.curColor = 'blue';
         searchForArticles($scope, $http, res);
+        searchForMedia($scope, $http, res);
     };
 
     $scope.addTrackedGame = function(game)
@@ -60,6 +68,61 @@ function searchForArticles($scope, $http, res)
     };
     $http.get('/getArticles', options).then(function(resp){
         $scope.newsArticles = resp.data;
+    });
+}
+
+function searchForMedia($scope, $http, res)
+{
+    var options = {
+        params:{
+            gameName: res.name
+        }
+    };
+    $http.get('/gameMedia', options).then(function(resp){
+        //Filter out the video id to use it for the thumbnail
+        var mediaDatas = [];
+
+        for(var i = 0; i < resp.data.length; i++) {
+            var respItem = resp.data[i];
+            var urlSplit = respItem.url.split('/');
+
+            //If this is a youtube video
+            if(urlSplit[2].indexOf('youtube.com') > -1) {
+                var QueryItems = function () {
+                    var query_string = {};
+                    var query = respItem.url;
+                    var vars = query.split("?");
+                    for (var i = 0; i < vars.length; i++) {
+                        var pair = vars[i].split("=");
+                        // If first entry with this name
+                        if (typeof query_string[pair[0]] === "undefined") {
+                            query_string[pair[0]] = decodeURIComponent(pair[1]);
+                            // If second entry with this name
+                        } else if (typeof query_string[pair[0]] === "string") {
+                            var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+                            query_string[pair[0]] = arr;
+                            // If third or later entry with this name
+                        } else {
+                            query_string[pair[0]].push(decodeURIComponent(pair[1]));
+                        }
+                    }
+                    return query_string;
+                }();
+
+                //Get the v parameter which is the youtube video id
+                var vidId = QueryItems.v;
+
+                var mediaData = {
+                    url: respItem.url,
+                    title: respItem.title,
+                    imgsrc: "https://img.youtube.com/vi/" + vidId + "/1.jpg"
+                };
+                mediaDatas.push(mediaData);
+            }
+
+        }
+        //TODO: Other media platforms besides youtube
+        $scope.mediaItems = mediaDatas;
     });
 }
 
