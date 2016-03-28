a5a833891ef6be37591d8ac4cb398051c9687d8d
Joseph Cruz <cruzj6@wit.edu>
2016-03-28 19:57:37 -0400

    d

--- DatabaseManager.js
+++ DatabaseManager.js
@@ -16,11 +16,12 @@ function addGameIDToUser(gameId, userId, doneCallback)
 {
         pg.defaults.ssl = true;
 
-        pg.connect(process.env.DATABASE_URL, function(err, client) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             if (err) throw err;
             console.log('Connected to postgres! Getting schemas...');
 
             client.query("CREATE TABLE if not exists tracked_games (userid TEXT, gameId TEXT);");
+            done();
         });
 
         //Make self call to get the tracked games for the user
@@ -30,11 +31,12 @@ function addGameIDToUser(gameId, userId, doneCallback)
             //If the game isn't already tracked by the user, add its
             if(!_.contains(ids, gameId))
             {
-                pg.connect(process.env.DATABASE_URL, function(err, client) {
+                pg.connect(process.env.DATABASE_URL, function(err, client, done) {
                     console.log('inserting data!!');
                     //Prep our query
                     client.query("INSERT INTO tracked_games VALUES ($1, $2);", [userId, gameId], function (err, res) {
                         doneCallback();
+                        done();
                     });
                 });
             }
@@ -44,12 +46,13 @@ function addGameIDToUser(gameId, userId, doneCallback)
 //Remove a game from the database for a userId
 function removeGameIDFromUser(gameId, userId, doneCallback)
 {
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 
         client.query("CREATE TABLE if not exists tracked_games (userid TEXT, gameId TEXT);");
 
         client.query("DELETE FROM tracked_games WHERE userid=($1) AND gameId=($2);", [userId, gameId], function(){
             doneCallback();
+            done();
         });
     });
 
@@ -58,7 +61,7 @@ function removeGameIDFromUser(gameId, userId, doneCallback)
 function getUsersTrackedGameIds(userid, handleUserIds)
 {
     console.log("Entered get user tracked games");
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query("CREATE TABLE if not exists tracked_games (userid TEXT, gameId TEXT);");
 
         console.log('GETTING USER TRACKED GAMES');
@@ -68,6 +71,7 @@ function getUsersTrackedGameIds(userid, handleUserIds)
             console.log("got FROM DATABASE: " + JSON.stringify(res.rows));
             //Send back the rows
             handleUserIds(res.rows);
+            done();
         });
     });
 }
