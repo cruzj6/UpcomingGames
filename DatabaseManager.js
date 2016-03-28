60e299cbfede4d2513c747f6746e28aebd3e8a38
Joseph Cruz <cruzj6@wit.edu>
2016-03-28 18:28:43 -0400

    postgres

--- DatabaseManager.js
+++ DatabaseManager.js
@@ -1,7 +1,7 @@
 /**
  * Created by Joey on 2/19/16.
  */
-var sqlite3 = require('sqlite3').verbose();
+var pg = require('pg');
 var _=require('underscore-node');
 
 //Public functions
@@ -14,62 +14,52 @@ module.exports = {
 //Add a gameId to the database for a user that they wish to track
 function addGameIDToUser(gameId, userId, doneCallback)
 {
-    var db = new sqlite3.Database('data.db');
-    db.serialize(function() {
+        pg.defaults.ssl = true;
 
-        //Create our table if it is not present
-        db.run("CREATE TABLE if not exists tracked_games (userid TEXT, gameId TEXT)");
+        pg.connect(process.env.DATABASE_URL, function(err, client) {
+            if (err) throw err;
+            console.log('Connected to postgres! Getting schemas...');
 
-        //Make self call to get the tracked games for the user
-        getUsersTrackedGameIds(userId, function(ids)
-        {
-            //If the game isn't already tracked by the user, add it
-            if(!_.contains(ids, gameId))
+            client.query("CREATE TABLE if not exists tracked_games (userid TEXT, gameId TEXT);");
+
+            //Make self call to get the tracked games for the user
+            getUsersTrackedGameIds(userId, function(ids)
             {
-                //Prep our query
-                var stmt = db.prepare("INSERT INTO tracked_games VALUES (?, ?)");
-
-                //Fill in user and gameId
-                stmt.run(userId, gameId);
-                stmt.finalize();
-            }
-            db.close();
-            doneCallback();
+                //If the game isn't already tracked by the user, add it
+                if(!_.contains(ids, gameId))
+                {
+                    //Prep our query
+                    client.query("INSERT INTO tracked_games VALUES (($1), ($2));",[userId, gameId], function(){
+                        doneCallback();
+                    });
+                }
+            });
         });
-    });
-}
+};
 
 //Remove a game from the database for a userId
 function removeGameIDFromUser(gameId, userId, doneCallback)
 {
-    var db = new sqlite3.Database('data.db');
-    db.serialize(function() {
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
 
-        db.run("CREATE TABLE if not exists tracked_games (userid TEXT, gameId TEXT)");
+        client.query("CREATE TABLE if not exists tracked_games (userid TEXT, gameId TEXT);");
 
-       var stmt = db.prepare("DELETE FROM tracked_games WHERE userid=(?) AND gameId=(?)")
-
-        //Fill in gameId to be removed and userId
-        stmt.run(userId, gameId);
-        stmt.finalize();
-        db.close();
-        doneCallback();
+        pg.query("DELETE FROM tracked_games WHERE userid=(($1)) AND gameId=($2);", [userId, gameId], function(){
+            doneCallback();
+        });
     });
+
 }
 
 function getUsersTrackedGameIds(userid, handleUserIds)
 {
-    var db = new sqlite3.Database('data.db');
-    db.serialize(function() {
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
 
         //Select all tracked gameId's for that userId
-        db.all("SELECT gameId FROM tracked_games WHERE userid=(?)", userid, function(err, rows){
-            //Send back the rows
-            handleUserIds(rows);
+        pg.query("SELECT gameId FROM tracked_games WHERE userid=(($1));", [userid])
+            .on('end', function(rows) {
+                //Send back the rows
+                handleUserIds(rows);
+            })
         });
-
-    });
-
-    //Close our database
-    db.close();
 }
