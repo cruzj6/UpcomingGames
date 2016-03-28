05241ecd6845971426f96f121a65227ce4314741
Joseph Cruz <cruzj6@wit.edu>
2016-03-28 17:41:37 -0400

    f

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ app.use(passport.session());
 //Set up our strategies, starting with steam strategy
 passport.use(new SteamStrategy({
         //TODO: CHANGE TO REAL URL ON DEPLOY
-        returnURL: 'http://localhost:3000/auth/steam/return',
-        realm: 'https://polar-ravine-80609.herokuapp.com/steam/return',
+        returnURL: 'https://polar-ravine-80609.herokuapp.com//auth/steam/return',
+        realm: 'https://polar-ravine-80609.herokuapp.com',
         apiKey: process.env.STEAM_API_KEY
     },
     function(identifier, profile, done) {
