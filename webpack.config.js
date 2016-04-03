2a768b6aefb315efb80cde14adbabf8ac00e000a
Joseph Cruz <cruzj6@wit.edu>
2016-04-03 14:09:10 -0400

    uglify and more UI

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: "./webvc/index.js",
     output: {
         path: __dirname + "/builds",
         filename: "bundle.js"
     },
+
     module: {
         loaders: [
             {
                 test: /.*\/app\/.*\.js$/,
+                exclude: /.spec.js/,
                 loader: "uglify"
             }
         ]
-    }
+    },
+
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+
 };
