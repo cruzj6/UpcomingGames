162b081585dc8dd821b87099f74d2cf677a88551
Joseph Cruz <cruzj6@wit.edu>
2016-04-05 21:47:04 -0400

    Visual upgrades

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ var gulp = require('gulp');
 var webpack = require('webpack-stream');
 const babel = require('gulp-babel');
 const runsequence = require('run-sequence');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./client/style/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./client/style'));
+});
 
 gulp.task('default', function(){
     return gulp.src('client/views/index.js')
