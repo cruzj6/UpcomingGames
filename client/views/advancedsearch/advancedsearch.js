43972f9be8f0e8f5902acd6f8a06b0daab7da634
Joseph Cruz <cruzj6@wit.edu>
2016-11-06 23:28:24 -0500

    Added advanced search tab, list of advanced search results, advanced search fields and button

--- client/views/advancedsearch/advancedsearch.js
+++ client/views/advancedsearch/advancedsearch.js
@@ -3,11 +3,25 @@ var app = angular.module('upcomingGames');
 app.controller('advancedsearch',function ($scope, httpReqService) {
 
   var vm = this;
+
+  //Init field defaults
+  vm.platform = "";
+  vm.month = "";
+  vm.year = "";
   vm.results = "No Results...";
+
+  //Perform an advanced search against the API
   vm.doSearch = function(){
-      httpReqService.getAdvancedSearch("xbone", 11, 2016, [], function(data){
-          vm.results = JSON.stringify(data);
+      httpReqService.getAdvancedSearch(vm.platform, vm.month, vm.year, [], function(data){
+          vm.results = data;
       });
+  }
+
+  //Add a game to tracked games
+  vm.addGame = function(gameid){
+    httpReqService.addTrackedGamePost(gameid, function(){
+      //TODO: Something on success?
+    });
   }
 
 });
