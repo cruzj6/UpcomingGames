f1ab049b5581c05af2fc7561bf0040ac4d7cdd19
Joey Cruz <cruzj6@wit.edu>
2016-11-24 12:31:44 -0500

    Added gulp watch capability to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,21 @@ const babel = require('gulp-babel');
 const runsequence = require('run-sequence');
 var sass = require('gulp-sass');
 
+gulp.task('default', ['watch']);
+
 gulp.task('sass', function () {
     return gulp.src('./client/style/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./client/style'));
 });
 
-gulp.task('default', function(){
-    return gulp.src('client/views/index.js')
+gulp.task('js', function(){
+     return gulp.src('client/views/index.js')
         .pipe(webpack(require('./webpack.config.js')))
         .pipe(gulp.dest('client/builds/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['client/**/*.js', '!client/builds/**/*.js'], ['js']);
+  gulp.watch('client/style/scss/**/*.scss', ['sass']);
 });
