00a3701f97aaf11ae7f5c15ed0e46f7a1a3aff63
Joseph Cruz <cruzj6@wit.edu>
2016-12-02 18:36:38 -0500

    Updated node module exports

--- server/libs/externalapis/newsAPI.js
+++ server/libs/externalapis/newsAPI.js
@@ -5,74 +5,58 @@
 //require('dotenv').config();
 var apiKey = process.env.BING_KEY;
 var rootUri = 'https://api.datamarket.azure.com/Bing/Search';
-var auth = new Buffer([ apiKey, apiKey ].join(':')).toString('base64');
+var auth = new Buffer([apiKey, apiKey].join(':')).toString('base64');
 var request = require('request').defaults({
-    headers:{
-        'Authorization' : 'Basic ' + auth
+    headers: {
+        'Authorization': 'Basic ' + auth
     }
 });
 //var Bing = require('node-bing-api')({accKey: apiKey});
 
-//Usable by requiring the module
-module.exports = {
-    getGameNews: function(gameName, callback) {
-        getGameNews(gameName, callback);
-    },
-    getGameMedia: function(gameName, mediaDataHandler)
-    {
-        getGameMedia(gameName, mediaDataHandler);
-    }
-
-};
-
 //Requests media data from the Bing web API
-function getGameMedia(gameName, mediaDataHandler)
-{
+export function getGameMedia(gameName, mediaDataHandler) {
     //We want video results
     var searchString = rootUri + '/Video';
 
     //Configure options and make request
     request.get({
         uri: searchString,
-        qs:{
-            $format:'json',
+        qs: {
+            $format: 'json',
             Query: "'" + gameName + "'"
         }
-    }, function(err, res, body)
-    {
-        //Init our results array we will send to callback
-        var resultsArray = [];
-        var jsonRes = JSON.parse(body);
-        var results = jsonRes.d.results;
+    }, function (err, res, body) {
+            //Init our results array we will send to callback
+            var resultsArray = [];
+            var jsonRes = JSON.parse(body);
+            var results = jsonRes.d.results;
 
-        //For each result from the BingAPI, format our callback response object
-        for(var i = 0; i < results.length; i++)
-        {
-            var curRes = results[i];
-            resultsArray.push({
-                title: curRes.Title,
-                url: curRes.MediaUrl,
-                thumbnail: curRes.Thumbnail
-            });
-        }
+            //For each result from the BingAPI, format our callback response object
+            for (var i = 0; i < results.length; i++) {
+                var curRes = results[i];
+                resultsArray.push({
+                    title: curRes.Title,
+                    url: curRes.MediaUrl,
+                    thumbnail: curRes.Thumbnail
+                });
+            }
 
-        //callback
-        mediaDataHandler(resultsArray);
-    });
+            //callback
+            mediaDataHandler(resultsArray);
+        });
 }
 
 //Request news articles from the BingAPI
-function getGameNews(gameName, callback)
-{
+export function getGameNews(gameName, callback) {
     //We want news
     var searchString = rootUri + '/News';
     request.get({
         uri: searchString,
-        qs:{
-            $format:'json',
+        qs: {
+            $format: 'json',
             Query: "'" + gameName + "'"
         }
-    }, function(err, res, body){
+    }, function (err, res, body) {
 
         //Init the callback response array
         var resultsArray = [];
@@ -81,7 +65,7 @@ function getGameNews(gameName, callback)
         var results = jsonRes.d.results;
 
         //Process each result into something usable for us
-        for(var i = 0; i < results.length;i++){
+        for (var i = 0; i < results.length; i++) {
             var curRes = results[i];
             resultsArray.push(
                 {
--- server/libs/externalapis/steamAPI.js
+++ server/libs/externalapis/steamAPI.js
@@ -5,71 +5,65 @@
 var request = require('request');
 var apiKey = process.env.STEAM_API_KEY;
 
-module.exports =
-{
-    getUserOwnedGames: function(userSteamId, handleOwnedGamesData) {
+export function getUserOwnedGames(userSteamId, handleOwnedGamesData) {
 
-        //Build steam API Query
-        var query =  "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
-            + apikey + "&include_appinfo=1&steamid=" + userSteamId + "&format=json";
+    //Build steam API Query
+    var query = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
+        + apikey + "&include_appinfo=1&steamid=" + userSteamId + "&format=json";
 
-        //Make our request
-        request.get(query, function(err, res, body){
-            var jsonData = JSON.parse(body);
-            var resp = jsonData.response;
+    //Make our request
+    request.get(query, function (err, res, body) {
+        var jsonData = JSON.parse(body);
+        var resp = jsonData.response;
 
-            //Get the games data
-            var games = resp.games;
+        //Get the games data
+        var games = resp.games;
 
-            //callback
-            handleOwnedGamesData(games);
-        });
-    },
-    //TODO: We can get steam friends tracked games (Future facebook etc too)!
-    getSteamFriends: function(userSteamId, handleFriendsData)
-    {
-        //Build query
-        var query =  "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?" +
-            "key=" + apiKey +
-            "&steamid=" + userSteamId +
-            "&relationship=friend";
+        //callback
+        handleOwnedGamesData(games);
+    });
+}
 
-        //Make request
-        request.get(query, function(err, res, body)
-        {
-            var jsonData = JSON.parse(body);
-            var friendsList = jsonData.friendslist;
+//TODO: We can get steam friends tracked games (Future facebook etc too)!
+export function getSteamFriends(userSteamId, handleFriendsData) {
+    //Build query
+    var query = "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?" +
+        "key=" + apiKey +
+        "&steamid=" + userSteamId +
+        "&relationship=friend";
 
-            //Get the Friends data
-            var friendsArray = friendsList.friends;
+    //Make request
+    request.get(query, function (err, res, body) {
+        var jsonData = JSON.parse(body);
+        var friendsList = jsonData.friendslist;
 
-            console.log('GetFriendList Request: ' + JSON.stringify(friendsArray));
+        //Get the Friends data
+        var friendsArray = friendsList.friends;
 
-            //Contains "steamid", "relationship" (ie. 'friend'), and "friend_since"
-            //for each friend
-            handleFriendsData(friendsArray);
-        });
-    },
-    getSteamUsersInfo: function(userSteamIdArray, handleFriendUserInfo)
-    {
-        //Build comma delimited list
-        var queryidstring = "";
-        for(var i=0; i < userSteamIdArray.length; i++)
-        {
-            queryidstring += userSteamIdArray[i] + ",";
-        }
-        console.log("Getting user info using query: " + queryidstring);
+        console.log('GetFriendList Request: ' + JSON.stringify(friendsArray));
 
-        //Build query
-        var query = " http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?" +
-            "key=" + apiKey +
-            "&steamids=" + queryidstring;
+        //Contains "steamid", "relationship" (ie. 'friend'), and "friend_since"
+        //for each friend
+        handleFriendsData(friendsArray);
+    });
+}
 
-        request.get(query, function(err, res, body)
-        {
-            var jsonData = JSON.parse(body);
-            console.log("Got all users data: " + JSON.stringify(jsonData));
-            handleFriendUserInfo(jsonData.response);
-        });
+export function getSteamUsersInfo(userSteamIdArray, handleFriendUserInfo) {
+    //Build comma delimited list
+    var queryidstring = "";
+    for (var i = 0; i < userSteamIdArray.length; i++) {
+        queryidstring += userSteamIdArray[i] + ",";
     }
-};
+    console.log("Getting user info using query: " + queryidstring);
+
+    //Build query
+    var query = " http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?" +
+        "key=" + apiKey +
+        "&steamids=" + queryidstring;
+
+    request.get(query, function (err, res, body) {
+        var jsonData = JSON.parse(body);
+        console.log("Got all users data: " + JSON.stringify(jsonData));
+        handleFriendUserInfo(jsonData.response);
+    });
+}
